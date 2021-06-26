c1c30a32ef24c9f494f637a64749ea4bf6bd0a02
noahe123 <eichlernoahj@gmail.com>
2021-06-26 16:25:32 -0700

    First Commit
    Added Project

---
+++ Freaky Forms Inspired/Assets/Scripts/DropShadow.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DropShadow : MonoBehaviour
+{
+    public Vector2 ShadowOffset;
+    public Material ShadowMaterial;
+
+    SpriteRenderer spriteRenderer;
+    GameObject shadowGameobject;
+
+    void Start()
+    {
+        //destroy old shadow if it exists
+        if (transform.childCount > 0)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //create a new gameobject to be used as drop shadow
+        shadowGameobject = new GameObject("Shadow");
+
+        //make object child
+        shadowGameobject.transform.parent = transform;
+
+        //set shadow scale
+        shadowGameobject.transform.localScale = Vector3.one;
+
+
+        //create a new SpriteRenderer for Shadow gameobject
+        SpriteRenderer shadowSpriteRenderer = shadowGameobject.AddComponent<SpriteRenderer>();
+
+        //set the shadow gameobject's sprite to the original sprite
+        shadowSpriteRenderer.sprite = spriteRenderer.sprite;
+        //set the shadow gameobject's material to the shadow material we created
+        shadowSpriteRenderer.material = ShadowMaterial;
+
+        //update the sorting layer of the shadow to always lie behind the sprite
+        shadowSpriteRenderer.sortingLayerName = spriteRenderer.sortingLayerName;
+        shadowSpriteRenderer.sortingOrder = spriteRenderer.sortingOrder - 1;
+
+        //set the shadow strength
+        shadowSpriteRenderer.color = new Color(0, 0, 0, .4f);
+
+
+
+    }
+  
+
+    void LateUpdate()
+    {
+        //update the position and rotation of the sprite's shadow with moving sprite
+        shadowGameobject.transform.position = transform.position + (Vector3)ShadowOffset;
+        shadowGameobject.transform.rotation = transform.rotation;
+    }
+}
---
+++ Freaky Forms Inspired/Assets/Scripts/NormalMapCustom.cs
@@ -0,0 +1,29 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class NormalMapCustom : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void SetNormalMap()
+    {
+        material.EnableKeyword("_NORMALMAP");
+
+        //fetch name of normal map
+        string normal = "Images/Body Parts/Normals" + "/" + spriteRenderer.sprite.name;
+
+        //Set the Normal map using the Texture
+        spriteRenderer.material.SetTexture("_BumpMap", Resources.Load(normal) as Texture2D);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
