3eaf3a6d552c0420a22cf9c2c29602fb82e593c0
noahe123 <eichlernoahj@gmail.com>
2021-06-29 03:05:48 -0700

    Worked on color picker

--- Freaky Forms Inspired/Assets/Scripts/ColorButton.cs
+++ Freaky Forms Inspired/Assets/Scripts/ColorButton.cs
@@ -13,6 +13,10 @@ public class ColorButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
     public GameObject colorCircle;
     public Color myColor;
 
+    bool isPressed;
+
+    GameObject objectOutlining;
+
 	void Start()
 	{
         myColor = transform.GetChild(1).GetComponent<Image>().color;
@@ -20,9 +24,42 @@ public class ColorButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 
         Button btn = GetComponent<Button>();
 	}
+
+    private void Update()
+    {
+
+        if (isPressed == true)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hits = Physics2D.GetRayIntersectionAll(ray, 1500f);
+
+
+                foreach (var hit in hits)
+                {
+                    if (hit.collider.name.Contains("Body Template Normals"))
+                    {
+                        objectOutlining = hit.collider.gameObject;
+                        objectOutlining.transform.parent.GetChild(1).gameObject.SetActive(true);
+                    }   
+                    else
+                    {
+                        if (objectOutlining != null)
+                        {
+                            objectOutlining.transform.parent.GetChild(1).gameObject.SetActive(false);
+                        }
+                    }
+                }
+
+ 
+
+        }
+
+    }
+
     //OnPointerDown is also required to receive OnPointerUp callbacks
     public void OnPointerDown(PointerEventData eventData)
     {
+        isPressed = true;
         colorCircle.GetComponent<Image>().color = myColor;
         colorCircle.GetComponent<Image>().enabled = true;
     }
@@ -30,6 +67,8 @@ public class ColorButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
     //Do this when the mouse click on this selectable UI object is released.
     public void OnPointerUp(PointerEventData eventData)
     {
+
+        isPressed = false;
         colorCircle.GetComponent<Image>().enabled = false;
 
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -40,7 +79,8 @@ public class ColorButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
                 if (hit.collider.name.Contains("Body Template Normals"))
                 {
                      hit.collider.gameObject.transform.parent.parent.gameObject.GetComponent<SpriteRenderer>().color = myColor;
-                }
+
             }
+        }
     }
 }
